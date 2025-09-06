4fb4f7fa57cda1804931a73d7641c4b506570cef
Henry Baten <hbatent@miumg.edu.gt>
2025-09-06 13:11:10 -0600

    committed

---
+++ src/routes/categorias.router.js
@@ -0,0 +1,6 @@
+const express=require('express');
+const router = express.Router();
+const controller=require('../controllers/categorias.controller');
+router.get('/',controller.index);
+router.get('/:id',controller.show);
+module.exports = router;
